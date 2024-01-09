553faf6aedcc64600d29084deea3e040815c3fdf
Rafael Silva e Silva <rafaelssilva335@gmail.com>
2024-01-09 09:45:51 -0300

    feat: Initial project setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const { gql, ApolloServer } = require("apollo-server");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const neo4j = require("neo4j-driver");
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
+require("dotenv").config();
+
+const typeDefs = gql`
+  type Movie {
+    title: String!
+    year: Int
+    plot: String
+    actors: [Person!]! @relationship(type: "ACTED_IN", direction: IN)
+  }
+
+  type Person {
+    name: String!
+    movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+  }
+`;
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+neoSchema.getSchema().then((schema) => {
+    const server = new ApolloServer({
+        schema: schema,
+        plugins: [
+          ApolloServerPluginLandingPageGraphQLPlayground({
+            settings: {
+              'editor.theme': 'light',
+            },
+          }),
+        ],
+    });
+
+    server.listen().then(({ url }) => {
+        console.log(`GraphQL server ready on ${url}`);
+    });
+});
